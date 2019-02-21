d77bd082f7436631b6d61a69b2bd1efe11260bf1
Ishaan Kumar <ffishaan@gmail.com>
2019-02-21 12:52:29 -0500

    updated constant.py

--- horoma/constants.py
+++ horoma/constants.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-
+from enum import Enum
 # data file paths
 DATA_ROOT_FOLDER = Path('/rap/jvb-000-aa/COURS2019/etudiants/data/horoma')
 TRAINING_DATA_FILE = DATA_ROOT_FOLDER / 'train_x.dat'
@@ -15,8 +15,18 @@ SAVED_MODEL_DIR = Path(os.path.abspath(PROJECT_ROOT / '../saved_models'))
 PCA_MODEL_DEFAULT_PATH = SAVED_MODEL_DIR / 'pca_model.sav'
 TSNE_MODEL_DEFAULT_PATH = SAVED_MODEL_DIR / 'tsne_model.sav'
 RESULT_DIR = Path(os.path.abspath(PROJECT_ROOT / '../results'))
+PARAM_DIR = PROJECT_ROOT / 'params'
 
 # data variables
 TRAINING_DATA_SIZE = 1614214
 VALIDATION_DATA_SIZE = 201778
 TEST_DATA_SIZE = 201778
+
+
+class TrainMode(Enum):
+    '''
+    Enum for training mode
+    '''
+    TRAIN_ALL = 0
+    TRAIN_ONLY_EMBEDDING = 1
+    TRAIN_ONLY_CLUSTER = 2
