9109815d8f2f7544e059722be370dfcf0064251d
Ishaan Kumar <ffishaan@gmail.com>
2019-02-21 23:51:25 -0500

    added ARI to metrics result

--- horoma/utils/score.py
+++ horoma/utils/score.py
@@ -2,10 +2,11 @@
 Module that contains scoring functions
 '''
 
-from sklearn.metrics import accuracy_score, f1_score
+from sklearn.metrics import accuracy_score, adjusted_rand_score, f1_score
 
 
 def compute_metrics(y_true, y_pred):
     accuracy = accuracy_score(y_true, y_pred)
     f1 = f1_score(y_true, y_pred, average="weighted")
-    return accuracy, f1
+    ari = adjusted_rand_score(y_true, y_pred)
+    return accuracy, f1, ari
