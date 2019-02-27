28e1e1c4e0b71658edd3bcfcfcbd11e0e8ccf1d4
David <david.abraham.1337@gmail.com>
2019-02-27 14:39:01 -0500

    Add layers to model

--- horoma/models/caes.py
+++ horoma/models/caes.py
@@ -26,7 +26,7 @@ class CDAutoEncoder(nn.Module):
         )
 
         self.criterion = nn.MSELoss()
-        self.optimizer = torch.optim.Adam(self.parameters(), lr=0.1)
+        self.optimizer = torch.optim.SGD(self.parameters(), lr=0.1)
         self.current_loss = 0
 
     def forward(self, x):
@@ -34,17 +34,17 @@ class CDAutoEncoder(nn.Module):
         x = x.detach()
         # Add noise, but use the original lossless input as the target.
         x_noisy = x * (x.data.new(x.size()).normal_(0, 0.1) > -.1).type_as(x)
+        y = self.forward_pass(x_noisy)
+
         if self.training:
-            y = self.forward_pass(x_noisy)
             x_reconstruct = self.backward_pass(y)
             x.requires_grad = False
             loss = self.criterion(x_reconstruct, x)
-            self.current_loss = loss
+            self.current_loss = loss.item()
             self.optimizer.zero_grad()
             loss.backward()
             self.optimizer.step()
-        else:
-            y = self.forward_pass(x)
+            
         return y.detach()
 
     def reconstruct(self, x):
@@ -60,33 +60,40 @@ class CAES(nn.Module):
     def __init__(self):
         super(CAES, self).__init__()
 
-        self.ae1 = CDAutoEncoder(3, 32, 2)
-        self.ae2 = CDAutoEncoder(32, 64, 2)
-        self.ae3 = CDAutoEncoder(64, 16, 2)
+        self.ae1 = CDAutoEncoder(3, 16, 2)
+        self.ae2 = CDAutoEncoder(16, 32, 2)
+        self.ae3 = CDAutoEncoder(32, 64, 2)
+        self.ae4 = CDAutoEncoder(64, 32, 2)
+        self.ae5 = CDAutoEncoder(32, 16, 2)
 
     def forward(self, x):
         a1 = self.ae1(x)
         a2 = self.ae2(a1)
         a3 = self.ae3(a2)
+        a4 = self.ae4(a3)
+        a5 = self.ae5(a4)
 
         if self.training:
-            return a3
+            return a5
 
         else:
-            return a3, self.reconstruct(a3)
+            return a5, self.reconstruct(a5)
 
     def reconstruct(self, x):
-        a2_reconstruct = self.ae3.reconstruct(x)
+        a4_reconstruct = self.ae5.reconstruct(x)
+        a3_reconstruct = self.ae4.reconstruct(a4_reconstruct)
+        a2_reconstruct = self.ae3.reconstruct(a3_reconstruct)
         a1_reconstruct = self.ae2.reconstruct(a2_reconstruct)
         x_reconstruct = self.ae1.reconstruct(a1_reconstruct)
         return x_reconstruct
     
     def get_loss(self):
-        return self.ae1.current_loss + self.ae2.current_loss + self.ae3.current_loss
-
+        return self.ae1.current_loss + self.ae2.current_loss + self.ae3.current_loss + self.ae4.current_loss + self.ae5.current_loss
+    
     def embedding(self, x):
         self.eval()
-        embedding, _ = self.forward(x)
-        embedding = embedding.view(embedding.shape[0], -1)
+        with torch.no_grad:
+            embedding, _ = self.forward(x)
+            embedding = embedding.view(embedding.shape[0], -1)
         self.train()
         return embedding.cpu().numpy()
