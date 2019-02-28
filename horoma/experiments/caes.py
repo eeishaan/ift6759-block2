bb8021a26994696dc5f59464ff342e4c48bd0c25
David <david.abraham.1337@gmail.com>
2019-02-28 14:07:15 -0500

    Import stacked autoencoder model and experiment

---
+++ horoma/experiments/caes.py
@@ -0,0 +1,50 @@
+from types import SimpleNamespace
+
+import numpy as np
+import torch
+import torch.nn.functional as F
+
+from horoma.cfg import DEVICE
+from horoma.constants import TrainMode
+from horoma.experiments import HoromaExperiment
+from horoma.utils.score import compute_metrics
+
+
+class CAESExperiment(HoromaExperiment):
+
+    def _train_embedding(self, train_loader, epochs, start_epoch, valid_train_loader, valid_valid_loader):
+        for epoch in range(start_epoch, epochs):
+
+            # first train embedding model
+            self._embedding_model.train()
+
+            # prepare context for hooks
+            ctx = SimpleNamespace(
+                epoch=epoch,
+                running_loss=0,
+                valid_train_loader=valid_train_loader,
+                valid_valid_loader=valid_valid_loader
+            )
+
+            self.before_train(ctx)
+            for _, data in enumerate(train_loader):
+
+                data = data.to(DEVICE)
+
+                # before_forwardp can add second layer of transformation
+                data = self.before_forwardp(ctx, data)
+
+                # zero out previous gradient
+                self._embedding_model.zero_grad()
+
+                outputs = self._embedding_model(data)
+                #loss = self.compute_loss(ctx, outputs, data)
+                ctx.running_loss += self._embedding_model.get_loss()
+                #loss.backward()
+                #self._embedding_optim.step()
+
+                self.after_forwardp(ctx, outputs, data)
+            self.after_train(ctx)
+
+    def before_train(self, ctx):
+        print("Starting epoch {}".format(ctx.epoch))
---
+++ horoma/models/caes.py
@@ -0,0 +1,99 @@
+import torch
+from torch import nn
+from torch.autograd import Variable
+import torch.nn.functional as F
+
+
+class CDAutoEncoder(nn.Module):
+    r"""
+    Convolutional denoising autoencoder layer for stacked autoencoders.
+    This module is automatically trained when in model.training is True.
+    Args:
+        input_size: The number of features in the input
+        output_size: The number of features to output
+        stride: Stride of the convolutional layers.
+    """
+    def __init__(self, input_size, output_size, stride):
+        super(CDAutoEncoder, self).__init__()
+
+        self.forward_pass = nn.Sequential(
+            nn.Conv2d(input_size, output_size, kernel_size=2, stride=stride, padding=0),
+            nn.ReLU(),
+        )
+        self.backward_pass = nn.Sequential(
+            nn.ConvTranspose2d(output_size, input_size, kernel_size=2, stride=stride, padding=0), 
+            nn.ReLU(),
+        )
+
+        self.criterion = nn.MSELoss()
+        self.optimizer = torch.optim.SGD(self.parameters(), lr=0.1)
+        self.current_loss = 0
+
+    def forward(self, x):
+        # Train each autoencoder individually
+        x = x.detach()
+        # Add noise, but use the original lossless input as the target.
+        x_noisy = x * (x.data.new(x.size()).normal_(0, 0.1) > -.1).type_as(x)
+        y = self.forward_pass(x_noisy)
+
+        if self.training:
+            x_reconstruct = self.backward_pass(y)
+            x.requires_grad = False
+            loss = self.criterion(x_reconstruct, x)
+            self.current_loss = loss.item()
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+            
+        return y.detach()
+
+    def reconstruct(self, x):
+        return self.backward_pass(x)
+
+
+class CAES(nn.Module):
+    r"""
+    A stacked autoencoder made from the convolutional denoising autoencoders.
+    Each autoencoder is trained independently and at the same time.
+    """
+
+    def __init__(self):
+        super(CAES, self).__init__()
+
+        self.ae1 = CDAutoEncoder(3, 16, 2)
+        self.ae2 = CDAutoEncoder(16, 32, 2)
+        self.ae3 = CDAutoEncoder(32, 64, 2)
+        self.ae4 = CDAutoEncoder(64, 32, 2)
+        self.ae5 = CDAutoEncoder(32, 16, 2)
+
+    def forward(self, x):
+        a1 = self.ae1(x)
+        a2 = self.ae2(a1)
+        a3 = self.ae3(a2)
+        a4 = self.ae4(a3)
+        a5 = self.ae5(a4)
+
+        if self.training:
+            return a5
+
+        else:
+            return a5, self.reconstruct(a5)
+
+    def reconstruct(self, x):
+        a4_reconstruct = self.ae5.reconstruct(x)
+        a3_reconstruct = self.ae4.reconstruct(a4_reconstruct)
+        a2_reconstruct = self.ae3.reconstruct(a3_reconstruct)
+        a1_reconstruct = self.ae2.reconstruct(a2_reconstruct)
+        x_reconstruct = self.ae1.reconstruct(a1_reconstruct)
+        return x_reconstruct
+    
+    def get_loss(self):
+        return self.ae1.current_loss + self.ae2.current_loss + self.ae3.current_loss + self.ae4.current_loss + self.ae5.current_loss
+    
+    def embedding(self, x):
+        self.eval()
+        with torch.no_grad:
+            embedding, _ = self.forward(x)
+            embedding = embedding.view(embedding.shape[0], -1)
+        self.train()
+return embedding.cpu().numpy()
