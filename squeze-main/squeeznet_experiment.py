7e775470bd52ffa11ca4ae9c68877c650852e88b
AlejandraJ <34015980+alejjimn@users.noreply.github.com>
2019-03-08 18:39:55 -0500

    Accuracy 13.75%

--- squeze-main/squeeznet_experiment.py
+++ squeze-main/squeeznet_experiment.py
@@ -27,7 +27,7 @@ from torch.utils.data import DataLoader
 from sklearn.cluster import KMeans
 from sklearn import warnings
 from sklearn.cluster import MiniBatchKMeans
-
+from PIL import Image
 DATA_ROOT_FOLDER = '/rap/jvb-000-aa/COURS2019/etudiants/data/horoma'
 
 
@@ -47,6 +47,14 @@ class HoromaDataset(Dataset):
             flattened: If True return the images in a flatten format.
             tranform: Transform data with supplied transformer
         """
+
+        self.transforms = torchvision.transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+
         self.transform = transform
         nb_channels = 3
         height = 32
@@ -101,6 +109,9 @@ class HoromaDataset(Dataset):
         if self.targets is not None:
             return self.transform(self.data[index]), torch.Tensor([self.targets[index]])
         return self.transform(self.data[index])
+        #    im2 = Image.new(im.mode, im.size)
+         #   return self.transform2(self.data[index]), self.transforms(im2.putdata([self.targets[index]]))
+        #return self.transform(self.data[index])
 
 
 class Fire(nn.Module):
@@ -271,7 +282,7 @@ class SqueezeImplement(object):
         # Train and evaluate
         model_ft, hist = self.train_model(model_ft, train_loader, optimizer_ft, self.num_epochs, True)
 
-        model_ft, hist = self.eval_model(model_ft, valid_loader, optimizer_ft, 5)
+        model_ft, hist = self.eval_model(model_ft, valid_loader, optimizer_ft)
 
 
     def train_model(self, model, train_loader, optimizer, num_epochs=25, use_kmeans=False):
@@ -359,18 +370,24 @@ class SqueezeImplement(object):
             time_elapsed = time.time() - since
             print('Time: {:.0f}m Loss: {:.4f} Acc: {:.4f}'.format(time_elapsed // 60, epoch_loss, epoch_acc))
 
+            # deep copy the model
+            if epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+                val_acc_history.append(epoch_acc)
+
         print()
 
         time_elapsed = time.time() - since
         print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
-        # print('Best val Acc: {:4f}'.format(best_acc))
+        print('Best val Acc: {:4f}'.format(best_acc))
 
         # load last model weights
-        best_model_wts = copy.deepcopy(model.state_dict())
+        #best_model_wts = copy.deepcopy(model.state_dict())
         model.load_state_dict(best_model_wts)
         return model, val_acc_history
 
-    def eval_model(self, model, valid_loader, optimizer, num_epochs=25):
+    def eval_model(self, model, valid_loader, optimizer):
         
         print("=================")
         print("Evaluating model...")
@@ -409,6 +426,9 @@ class SqueezeImplement(object):
 
                 _, preds = torch.max(outputs, 1)
 
+           # statistics
+            running_loss += loss.item() * inputs.size(0)
+            running_corrects += torch.sum(preds == labels.data)
 
         epoch_acc = running_corrects.double() / len(valid_loader.dataset)
         print("Loader len: ", len(valid_loader.dataset))
@@ -459,3 +479,16 @@ if __name__ == '__main__':
 
     squeezimp=SqueezeImplement()
     squeezimp.train()
+
+  #  train_dataset = HoromaDataset(split='train', transform=transforms.ToTensor())
+  #  valid_dataset = HoromaDataset(split='valid', transform=transforms.ToTensor())
+  #  train_overlapped_ds = HoromaDataset(split='train_overlapped', transform=transforms.ToTensor())
+  #  valid_overlapped_ds = HoromaDataset(split='valid_overlapped', transform=transforms.ToTensor())
+
+  #  train_loader = DataLoader(train_dataset, batch_size=100, shuffle=True)
+  #  valid_loader = DataLoader(valid_dataset, batch_size=100, shuffle=False)
+  #  train_overlapped = DataLoader(train_overlapped_ds, batch_size=100, shuffle=False)
+  #  valid_overlapped = DataLoader(valid_overlapped_ds, batch_size=100, shuffle=False)
+
+    #for inputs,labels in valid_loader:
+     #   print(inputs)
