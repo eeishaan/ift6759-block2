07807418e38f8f797205decf55b7a3c7145e7b16
AlejandraJ <34015980+alejjimn@users.noreply.github.com>
2019-03-12 20:04:29 -0400

    adding squeezenet

--- horoma/experiments/factory.py
+++ horoma/experiments/factory.py
@@ -1,12 +1,14 @@
 from horoma.experiments.ae import AEExperiment, VAEExperiment
 from horoma.experiments.cae import CAEExperiment
 from horoma.experiments.caes import CAESExperiment
+from horoma.experiments.squeezenet import SqueezenetExperiment
 
 SUPPORTED_EXP = {
     'ae': AEExperiment,
     'vae': VAEExperiment,
     'cae': CAEExperiment,
-    'caes': CAESExperiment
+    'caes': CAESExperiment,
+    'squeezenet': SqueezenetExperiment
 }
 
 
